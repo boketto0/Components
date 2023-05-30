c1ed4a7d42e2c53f0d3fa48a2990dfbbc39063c7
boketto0 <bekmina.julia@gmail.com>
2023-05-30 21:33:05 +0300

    lending

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import './Loader.css'
+
+export default function Loader (props) {
+    return(
+        <div>
+            <div className ='lds-circle'>
+                <div></div>
+            </div>
+        </div>
+    )
+}
