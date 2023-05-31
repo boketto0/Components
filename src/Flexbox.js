ee81f83b71c9b7e7110e5843a83cc9660c6d28fb
boketto0 <bekmina.julia@gmail.com>
2023-05-31 18:35:54 +0300

    flexbox

---
+++ src/Flexbox.js
@@ -0,0 +1,19 @@
+import './Flexbox.css'
+
+export function Flexbox() {
+
+    return(
+        <div className="container">
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+        </div>
+    )
+}
