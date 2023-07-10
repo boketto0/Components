19d2c1d73e9ffdbecf9a89e880e0ca35a115e9ad
boketto0 <bekmina.julia@gmail.com>
2023-07-10 23:07:27 +0300

    Fixing size switch

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import './Switch.css';
-import classnames from 'classnames'
+import classnames from 'classnames';
 
 export const SwitchSize = {
   SMALL: 'small',
@@ -27,10 +27,20 @@ export const Switch = (props) => {
     setIsDragging(false);
   }, []);
 
+  const sliderClassName = classnames('slider', {
+    'slider-small': size === SwitchSize.SMALL,
+    'slider-large': size === SwitchSize.LARGE,
+    'slider-disabled': disabled
+  });
+
   return (
-    <label className={`switch switch-${size} ${checked ? 'switch-on' : 'switch-off'} ${isDragging ? 'slider-dragging' : ''}`} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+    <label
+      className={`switch switch-${size} ${checked ? 'switch-on' : 'switch-off'} ${isDragging ? 'slider-dragging' : ''}`}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
       <input type="checkbox" checked={checked} onChange={handleCheckboxChange} disabled={disabled} />
-      <span className={classnames('slider', `slider-${size}`, { 'slider-disabled': disabled })}>
+      <span className={sliderClassName}>
         {checked ? onIcon : offIcon}
       </span>
     </label>
@@ -41,5 +51,7 @@ Switch.propTypes = {
   checked: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
   disabled: PropTypes.bool,
-  size: PropTypes.oneOf(Object.values(SwitchSize))
+  size: PropTypes.oneOf(Object.values(SwitchSize)),
+  onIcon: PropTypes.node,
+  offIcon: PropTypes.node
 };
