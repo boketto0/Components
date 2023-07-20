5cd5067ca4d26608f1c13e836224aeaebddd69df
boketto0 <bekmina.julia@gmail.com>
2023-07-20 15:09:06 +0300

    Fixing navigation ComponentsOverview

--- src/components/ComponentsOverview.js
+++ src/components/ComponentsOverview.js
@@ -1,84 +1,61 @@
 import './ComponentsOverview.css';
-import { Card, CardType } from './cards/Card'; // Обновленный импорт
+import { Card, CardType } from './cards/Card';
 import { CardWrapper } from './cards/CardWrapper';
 import { Button, ButtonSize, ButtonType } from './button/Button';
 import { Checkbox } from './checkbox';
 import { CheckboxStory2 } from './checkboxStories';
 import { SwitchStory2 } from './switchStories';
 import { SwitchStory4 } from './switchStories';
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
 
 export const ComponenetsOverview = () => {
 
-    function hanldePictureClick() {
-
-    }
-
   const componentsStory = [
     {
-      title: 'Button',
-      component: 
-      <div className='card-component'>
-        <Button size={ButtonSize.MEDIUM} type={ButtonType.PRIMARY} text={'Button'}/>
-        <Button size={ButtonSize.LARGE} type={ButtonType.SECONDARY} text={'Button'}/>
-        <Button type={ButtonType.TEXT} text={'Button'}/>
-      </div>
-    },
-    {
-      title: 'Switch',
-      component: 
+      title: 'button',
+      component: (
         <div className='card-component'>
-            <SwitchStory2/>
-            <SwitchStory4/>
+          <Button size={ButtonSize.MEDIUM} type={ButtonType.PRIMARY} text={'Button'} />
+          <Button size={ButtonSize.LARGE} type={ButtonType.SECONDARY} text={'Button'} />
+          <Button type={ButtonType.TEXT} text={'Button'} />
         </div>
+      )
     },
     {
-      title: 'Checkbox',
-      component: 
-      <div className='card-component'>
-          <Checkbox text={'Check all'}/>
-          <CheckboxStory2/>
-      </div>
+      title: 'switch',
+      component: (
+        <div className='card-component'>
+          <SwitchStory2 />
+          <SwitchStory4 />
+        </div>
+      )
+    },
+    {
+      title: 'checkbox',
+      component: (
+        <div className='card-component'>
+          <Checkbox text={'Check all'} />
+          <CheckboxStory2 />
+        </div>
+      )
     }
   ];
 
   return (
     <div className='components-wrapper'>
-        <div className='components-list'>
-            <div className="par-1">Components Overview</div>
-            <div className="par-2">Components</div>
-        </div>
-        <CardWrapper>
-          {componentsStory.map((el) => {
-            return (
-              <div>
-                <Card cardType={CardType.SECOND} title={el.title} component={el.component} />
-              </div>
-            );
-          })}
-        </CardWrapper>
+      <div className='components-list'>
+        <div className="par-1">Components Overview</div>
+        <div className="par-2">Components</div>
+      </div>
+      <CardWrapper className='components-list a'>
+        {componentsStory.map((el) => (
+          <div key={el.title}>
+            <Link to={`/${el.title}`}>
+              <Card cardType={CardType.SECOND} title={el.title} component={el.component} />
+            </Link>
+          </div>
+        ))}
+      </CardWrapper>
     </div>
   );
-}
-
-// Router>
-//         <ul>
-//           <li>
-//             <a href="/">Home</a>
-//           </li>
-//           <li>
-//             <a href="/button">Button</a>
-//           </li>
-//           <li>
-//             <a href="/checkbox">Checkbox</a>
-//           </li>
-//           <li>
-//             <a href="/switch">Switch</a>
-//           </li>
-//         </ul>
-//         <Routes className="component-wrapper">
-//           <Route className='wrapper' path="/" element={<ComponenetsOverview/>} />
-//           <Route className='wrapper' path="/checkbox" element={<CheckboxStory />} />
-//           <Route path="/switch" element={<SwitchStory/>}/>
-//           <Route path="/button" element={<ButtonStory/>}/>
-//         </Routes>
-//       </Router>
+};
